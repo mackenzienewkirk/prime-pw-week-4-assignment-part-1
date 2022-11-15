94ac993f411b8da319532040bc7ca692ea95db0d
Mackenzie Newkirk <mackenzie.joy.newkirk@gmail.com>
2022-11-15 13:28:31 -0600

    Hello Kenzie

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -14,11 +14,11 @@ console.log('Test - should say "Hello World!"', hello());
 
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
-function helloName( name ) {
-  return;
+function helloName( Kenzie ) {
+  return 'Hello, Kenzie!';
 }
 // Remember to call the function to test
-
+console.log('Test - should say "Hello, Kenzie!"', helloName());
 
 // 3. Function to add two numbers together & return the result
 function addNumbers( firstNumber ) {
